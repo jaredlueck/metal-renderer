69ab4c67b9d1ef917bf9c3637f603f6771b8d728
Jared Lueck <jared.lueck@hotmail.com>
2025-12-08 23:27:02 -0500

    Initial Commit

---
+++ metal-swift-new/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  metal-swift-new
+//
+//  Created by Jared Lueck on 2025-12-08.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
